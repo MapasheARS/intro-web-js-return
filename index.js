d19214df6a00d9c145dcacc7a92df3cae765acb1
Andrea <andrea.ramos.soto@angloaleman.edu.mx>
2022-10-28 11:53:19 -0500

    Actualizacion, return

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 
 // Crea la funcion suma
-
-
+function suma(4, 5) {
+    let suma = 4 + 5;
+    return suma
+};
 // Crea la funcion resta
-
-
+function resta(4, 5){
+    let resta = 5 - 4
+    return resta
+};
 // Crea la funcion numeroAleatorio
-
-
+function numeroAleatorio(4, 5) {
+    Math.random(4,5)
+    return Math.random
+}
 // Crea la funcion saludo
+function saludo(){
+    let nombre = "Elena"
+    return "hola + nombre"
+}
